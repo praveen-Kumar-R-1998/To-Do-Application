071d5daac7950601996f98cbe9458645eeb197b8
praveen <praveenravichandran2051@gmail.com>
2024-10-20 01:27:39 +0530

    commit

--- src/component/ToDo/ToDoApp.jsx
+++ src/component/ToDo/ToDoApp.jsx
@@ -213,62 +213,25 @@ function ListToDoComponent() {
 
 function HeaderComponent() {
   return (
-    <header className="header">
-      <div className="container">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href="https://github.com/praveen-Kumar-R-1998"
-            >
-              Developers profile
-            </a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/welcome/Praveen">
-              Home
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link className="nav-link" to="/todos">
-              ToDo's
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link className="nav-link" to="/logout">
-              Logout
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Login
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </header>
-
-    // <header className="border-bottom border-light border-5 mb-5 p-2">
-    //         <div className="container">
-    //             <div className="row">
-    //                 <nav className="navbar navbar-expand-lg">
-    //                     <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://github.com/praveen-Kumar-R-1998">Profile</a>
-    //                     <div className="collapse navbar-collapse">
-    //                         <ul className="navbar-nav">
-    //                             <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/Praveen">Home</Link></li>
-    //                             <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
-    //                         </ul>
-    //                     </div>
-    //                     <ul className="navbar-nav">
-    //                         <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
-    //                         <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
-    //                     </ul>
-    //                 </nav>
-    //             </div>
-    //         </div>
-    //     </header>
+    <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://github.com/praveen-Kumar-R-1998">Profile</a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/Praveen">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
   );
 }
 
